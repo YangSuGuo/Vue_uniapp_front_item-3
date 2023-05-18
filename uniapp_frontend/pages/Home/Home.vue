971e726266e6d18d8085f81e811f987e2f5bd512
YSG <YSG@git.com>
2023-05-18 23:31:16 +0800

    更新接口，vue网页卡片初步完成

---
+++ uniapp_frontend/pages/Home/Home.vue
@@ -0,0 +1,18 @@
+<template>
+	<view>home</view>
+</template>
+
+<script>
+export default {
+	data() {
+		return {};
+	},
+	methods: {}
+};
+</script>
+
+<style>
+page {
+	background-color: #ededed;
+}
+</style>
